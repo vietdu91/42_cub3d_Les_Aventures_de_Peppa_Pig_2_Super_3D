55f4b713a2d3bdaafd8ad43ddedb9d7c3e8f68cc
QnYosa <dimitriyoula@gmail.com>
2022-06-26 17:39:37 +0200

    lol

--- objs/parsing/collect_map.d
+++
@@ -1,6 +0,0 @@
-objs/parsing/collect_map.o: srcs/parsing/collect_map.c \
-  srcs/parsing/../../includes/cub3D.h includes/../mlx/mlx.h \
-  includes/./init.h includes/./game.h includes/./free.h \
-  includes/./utils.h includes/./errors.h includes/./check.h \
-  includes/./map.h includes/./maths.h includes/./parsing.h \
-  includes/raycasting.h
