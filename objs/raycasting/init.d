1cacbb441322e75c68b2d24f3f9782fd0660f753
Emmanuel TRAN <emtran@e2r4p18.42.fr>
2022-07-03 11:23:35 +0200

    last ligne droite

--- objs/raycasting/init.d
+++
@@ -1,6 +0,0 @@
-objs/raycasting/init.o: srcs/raycasting/init.c \
-  includes/../includes/cub3D.h includes/../mlx/mlx.h includes/./init.h \
-  includes/./game.h includes/./sprites.h includes/./free.h \
-  includes/./images.h includes/./utils.h includes/./errors.h \
-  includes/./check.h includes/./map.h includes/./maths.h \
-  includes/./parsing.h includes/raycasting.h
--- objs/utils/utils_is2.d
+++
@@ -1,6 +0,0 @@
-objs/utils/utils_is2.o: srcs/utils/utils_is2.c \
-  srcs/utils/../../includes/cub3D.h includes/../mlx/mlx.h \
-  includes/./init.h includes/./game.h includes/./sprites.h \
-  includes/./free.h includes/./images.h includes/./utils.h \
-  includes/./errors.h includes/./check.h includes/./map.h \
-  includes/./maths.h includes/./parsing.h includes/raycasting.h
